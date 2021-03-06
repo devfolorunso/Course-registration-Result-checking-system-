3302fa1392d71efc80fff09f4025d1118630b9cf
Afolorunso <adesanyafolorunso1@gmail.com>
2021-03-06 18:21:02 +0100

    CR-System --Recommited

--- Login.php
+++ Login.php
@@ -1,4 +1,4 @@
-<?php require_once 'core/init.php';?>
+<?php require_once 'core/init.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,118 +24,102 @@
 
 <body class="bg-gradient-primary">
 
-  <div class="container">
+  <div class="container" style="margin-top: 15%;">
 
-<!-- Outer Row -->
-<div class="row justify-content-center">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-  <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="col-xl-10 col-lg-12 col-md-9">
 
-<div class="card o-hidden border-0 shadow-lg my-5">
-<div class="card-body p-0">
-  <!-- Nested Row within Card Body -->
-  <div class="row">
-    <div class="col-lg-2"></div>
-    <div class="col-lg-8">
-      <div class="p-5">
-        <div class="text-center">
-                
-<?php
-if (Session::exists('login'))
-{
-  echo '<h1 class="h4 text-gray-900 mb-4"> Welcome Back! ' . Session::flash('login') .'</h1>';       
-}   
-  if (Input::exists())
-  {
-    if (Token::check(Input::get('token')))
-    {    
-      $validate = new  Validate();
-      $validation = $validate->check($_POST, array(
-      'username' => array('required' => true),
-      'password' => array('required' => true)
-     ));
+        <div class="card o-hidden border-0 shadow-lg my-5 mx-5" style="margin-top: 10%;">
+          <div class="card-body p-0 mx-5" style="margin-top: 5%;">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-2"></div>
+              <div class="col-lg-8">
+                <div class="p-5">
+                  <div class="text-center">
 
-     if ($validation->passed())
-     {
-     
-      $user = new User();
-      $remember = (Input::get('remember')==='on') ? true : false;
-      $login = $user->login(Input::get('username'),  Input::get('password'), $remember);
+                    <?php
+                    if (Session::exists('login')) {
+                      echo '<h1 class="h4 text-gray-900 mb-4"> Welcome Back! ' . Session::flash('login') . '</h1>';
+                    }
+                    if (Input::exists()) {
+                      if (Token::check(Input::get('token'))) {
+                        $validate = new  Validate();
+                        $validation = $validate->check($_POST, array(
+                          'username' => array('required' => true),
+                          'password' => array('required' => true)
+                        ));
+
+                        if ($validation->passed()) {
+
+                          $user = new User();
+                          $remember = (Input::get('remember') === 'on') ? true : false;
+                          $login = $user->login(Input::get('username'),  Input::get('password'), $remember);
 
 
-  if($login){
+                          if ($login) {
 
-  Session::flash('complete', 'Welcome! Kindly Complete Your Profile');
+                            Session::flash('complete', 'Welcome! Kindly Complete Your Profile');
 
-    if ($user->hasPermission('admin'))
-    {
-        redirect::to('admin/index.php');
-    }
-    else
-    {
-        redirect::to('complete-profile.php');
-    }        
-
-    }
-    else
-    {
-    echo '<div class="alert alert-danger text-center" role="alert">
+                            if ($user->hasPermission('admin')) {
+                              redirect::to('admin/index.php');
+                            } else {
+                              redirect::to('complete-profile.php');
+                            }
+                          } else {
+                            echo '<div class="alert alert-danger text-center" role="alert">
       <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
       <span class="sr-only">Error:</span>
     Sorry, Login Failed!! </div>';
-  }
-
-}
-else
-  {
-    foreach ($validation->errors() as $error)
-  {
-    echo'<div class="alert alert-danger text-capitalize text-center" role="alert">
+                          }
+                        } else {
+                          foreach ($validation->errors() as $error) {
+                            echo '<div class="alert alert-danger text-capitalize text-center" role="alert">
     <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
     <span class="sr-only">Error:</span>' .
-    $error . '</div>';
-  
-  }
-  }
+                              $error . '</div>';
+                          }
+                        }
+                      }
+                    }
+                    ?>
+                  </div>
+                  <form method="post" class="user">
 
-  }
-}
-?>
-      </div>
-        <form method="post" class="user">
-          
-          <div class="form-group">
-            <input type="email" class="form-control form-control-user"  name="username" placeholder="Enter Email Address...">
-          </div>
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" name="username" placeholder="Enter Email Address...">
+                    </div>
 
-          <div class="form-group">
-            <input type="password" class="form-control form-control-user" name="password" placeholder="Password">
-          </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" name="password" placeholder="Password">
+                    </div>
 
 
-           <div class="form-group">
-            <div class="custom-control custom-checkbox small">
-              <input type="checkbox" class="custom-control-input" name="remember" id="customCheck" >
-              <label class="custom-control-label" for="customCheck">Remember Me</label>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" name="remember" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+
+                    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+
+                    <input type="submit" value="Login" class="btn btn-primary btn-user btn-block  bg-gradient-primary">
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.php">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.php">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          
-          <input type="hidden" name="token" value="<?php echo Token::generate();?>">
-
-          <input  type="submit" value="Login" class="btn btn-primary btn-user btn-block  bg-gradient-primary">
-        </form>
-        <hr>
-        <div class="text-center">
-          <a class="small" href="forgot-password.php">Forgot Password?</a>
         </div>
-        <div class="text-center">
-          <a class="small" href="register.php">Create an Account!</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
 
       </div>
 
---
+++ README.md
@@ -0,0 +1,1 @@
+#Course Registration & Result Checking System
